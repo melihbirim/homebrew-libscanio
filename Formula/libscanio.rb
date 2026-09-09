60bd0da9642a4a76dec3ee68584a57abd25c55bf
Melih Birim <398797+melihbirim@users.noreply.github.com>
2026-09-09 09:50:01 +0100

    chore: update formula to v0.2.0

--- Formula/libscanio.rb
+++ Formula/libscanio.rb
@@ -1,24 +1,24 @@
 class Libscanio < Formula
   desc "Scan huge CSV files without loading them into memory"
   homepage "https://github.com/melihbirim/libscanio"
-  version "0.1.2"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/melihbirim/libscanio/releases/download/v0.1.2/scanio-macos-aarch64.tar.gz"
-      sha256 "961a678fb8dcb079cefe68dc5913a5c6201450c2b1aa4c737e810588230d2d6d"
+      url "https://github.com/melihbirim/libscanio/releases/download/v0.2.0/scanio-macos-aarch64.tar.gz"
+      sha256 "059f9051916ef0c19b88b8a955b3337a2122b5dd8d541162ef84de623856d342"
     end
     on_intel do
-      url "https://github.com/melihbirim/libscanio/releases/download/v0.1.2/scanio-macos-x86_64.tar.gz"
-      sha256 "34d08f98f968fa74576a61e4925363a2fb2e337af60e8ddd921895c910b2be82"
+      url "https://github.com/melihbirim/libscanio/releases/download/v0.2.0/scanio-macos-x86_64.tar.gz"
+      sha256 "95966c130d51c9530d1d0c769c861a451cb9b22437c11da9cad9df6f2894a9e9"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/melihbirim/libscanio/releases/download/v0.1.2/scanio-linux-x86_64.tar.gz"
-      sha256 "f75efb368a178db6e8fbbd134a7e7589a8bfa3a5676242f63b15cfdb93d27f85"
+      url "https://github.com/melihbirim/libscanio/releases/download/v0.2.0/scanio-linux-x86_64.tar.gz"
+      sha256 "b7fbe11cbe38f2e1385dfc406c522a1d8ca8fb757803bab5960f841873af5a8f"
     end
   end
 
